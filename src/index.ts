ce4ea6999a7a809e0e8a39bd1c4d6a7f8cf1fe60
Tusharshrivastav <ts3657@gmail.com>
2021-11-06 10:18:09 +0530

    setup typscrpt node and graphql

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql'
+import cors from 'cors'
+import { createConnection } from 'typeorm'
+
+const main = async () => {
+    const app = express()
+    app.use(cors())
+    app.use(express.json())
+    // app.use("/graphql", graphqlHTTP({
+    //     schema,
+    //     graphiql: true
+    // }))
+    app.listen(3001, () => {
+        console.log("listening on")
+    })
+}
+
+main().catch((err) => {
+    console.log(err)
+})
